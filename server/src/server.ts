6cd5710b1fcfa9b8c368911b54d4e9adf9e74100
maxmii <morrm046@gmail.com>
2023-11-15 23:35:03 +0000

    Setup express server

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config';
+import gameRoutes from './routes';
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 4000;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
+app.use(cors());
+app.use(express.json());
+app.use(gameRoutes);
+
+mongoose
+    .connect(uri)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+    })
+    .catch((error) => {
+        throw error;
+    });
