600c20750451c47b2029bf7f90b3e46518fcada0
maxmii <morrm046@gmail.com>
2023-11-15 23:37:04 +0000

    Create API controllers for Get all games Add a game Update game Delete a game

---
+++ server/src/controllers/games/index.ts
@@ -0,0 +1,93 @@
+import { Response, Request } from 'express';
+import { IGame } from './../../types/game';
+import Game from '../../models/game';
+
+const getGames = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const games: IGame[] = await Game.find();
+        res.status(200).json({ games });
+    } catch (error) {
+        throw error;
+    }
+};
+
+const addGame = async (req: Request, res: Response): Promise<void> => {
+    try {
+        console.log(req);
+        const body = req.body as Pick<
+            IGame,
+            | 'name'
+            | 'description'
+            | 'released'
+            | 'releaseDate'
+            | 'platforms'
+            | 'developer'
+            | 'genres'
+            | 'series'
+        >;
+        const game: IGame = new Game({
+            name: body.name,
+            description: body.description,
+            released: body.released,
+            releaseDate: body.releaseDate,
+            platforms: body.platforms,
+            developer: body.developer,
+            genres: body.genres,
+            series: body.series,
+        });
+
+        const newGame: IGame = await game.save();
+        const allGames: IGame[] = await Game.find();
+
+        res.status(201).json({
+            message: 'Game Added',
+            game: newGame,
+            games: allGames,
+        });
+    } catch (error) {
+        throw error;
+    }
+};
+
+const updateGame = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            params: { id },
+            body,
+        } = req;
+
+        const updateGame: IGame | null = await Game.findByIdAndUpdate(
+            { _id: id },
+            body,
+        );
+
+        const allGames: IGame[] = await Game.find();
+
+        res.status(200).json({
+            message: 'Game Updated',
+            game: updateGame,
+            games: allGames,
+        });
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteGame = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const deletedGame: IGame | null = await Game.findByIdAndRemove(
+            req.params.id,
+        );
+        const allGames: IGame[] = await Game.find();
+
+        res.status(200).json({
+            message: 'Game Delete',
+            game: deleteGame,
+            games: allGames,
+        });
+    } catch (error) {
+        throw error;
+    }
+};
+
+export { getGames, addGame, updateGame, deleteGame };
