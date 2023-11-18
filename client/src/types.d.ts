4b066246001557ff9b6e9c7b4d184fddd74a5cdd
maxmii <morrm046@gmail.com>
2023-11-18 00:23:50 +0000

    Update game type

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,22 +1,24 @@
-interface Game {
-  _id: string;
-  name: string;
-  description: string;
-  released: boolean;
-  releaseDate: Date;
-  platforms: string[];
-  developer: string;
-  genres: string[];
-  series: string;
+interface IGame {
+    _id: string;
+    name: string;
+    description: string;
+    released: boolean;
+    releaseDate: Date;
+    platforms: string[];
+    developer: string;
+    genres?: string[];
+    series?: string;
+    createdAt?: string;
+    updatedAt?: string;
 }
 
 interface GameProps {
-  game: Game;
+    game: IGame;
 }
 
 type ApiDataType = {
-  message: string;
-  status: string;
-  games: Game[];
-  game?: Game;
+    message: string;
+    status: string;
+    games: IGame[];
+    game?: IGame;
 };
