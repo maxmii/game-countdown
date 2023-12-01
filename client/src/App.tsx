88f1890bd8bd1b8fa19dbd9244d0c5d7bc4080bd
maxamills <morrm046@gmail.com>
2023-12-01 10:58:09 +0000

    Update App to show My Games

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { getGames } from './Api';
+import GameItem from './components/GameItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const [games, setGames] = useState<IGame[]>([]);
+
+    useEffect(() => {
+        fetchGames();
+    }, []);
+
+    const fetchGames = (): void => {
+        getGames()
+            .then(({ data: { games } }: IGame[] | any) => setGames(games))
+            .catch((err: Error) => console.log(err));
+    };
+
+    return (
+        <main className="App">
+            <h1>My Games</h1>
+            {games.map((game: IGame) => (
+                <GameItem key={game._id} game={game} />
+            ))}
+        </main>
+    );
+};
 
 export default App;
