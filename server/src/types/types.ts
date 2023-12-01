15ea6df1b075bf8ed52402dea6945394c07077fd
maxamills <morrm046@gmail.com>
2023-12-01 15:54:01 +0000

    Update types and remove intefaces Version and Console

--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -1,37 +1,24 @@
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export interface IGame extends Document {
     name: string;
-    description: string;
+    description?: string;
     released: boolean;
-    releaseDate: Date;
-    platforms: string[];
-    developer: string;
-    genres: string[];
-    series: string;
-}
-
-export interface IConsole extends Document {
-    name: string;
-    releaseDate: Date;
-    productFamily: string;
-    developer: string;
-    generation: string;
+    releaseDate?: Date;
+    platforms?: string[];
+    developer?: string;
+    genres?: string[];
+    series?: string;
 }
 
 export interface IUser extends Document {
     userName: string;
     password: string;
     dateOfBirth: Date;
-    gameListId: number;
-}
-
-export interface IVersion extends Document {
-    gameId: number;
-    consoleId: number;
+    gameList: Types.ObjectId;
 }
 
 export interface IGameList extends Document {
-    versionId: number;
-    userId: number;
+    game: Types.ObjectId;
+    user: Types.ObjectId;
 }
