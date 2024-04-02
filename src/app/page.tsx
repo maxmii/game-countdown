68323f428ab19800b65776171bfbd86e9f6b221f
Max Morris <morrm046@gmail.com>
2024-04-02 23:17:31 +0100

    Remove Nav from main app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
 
-import Nav from './components/layout/MainNavigation';
 
 export default function Home() {
   return (
     <>
-      <Nav />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Link href="/gamesTable">Games Table</Link>
       </main>
