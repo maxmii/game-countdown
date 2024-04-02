5ed8e11298a0df8f902a1cf8a3609604ed159a3c
Max Morris <morrm046@gmail.com>
2024-04-02 23:22:17 +0100

    Update MainNavigation and add css styling

--- src/app/components/layout/MainNavigation.tsx
+++ src/app/components/layout/MainNavigation.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { memo } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
@@ -9,18 +9,15 @@ const navLinks = [
   { title: 'About', path: '/about' },
   { title: 'Contact', path: '/contact' },
 ];
+
 const Nav: React.FC = () => {
   const pathName = usePathname();
   return (
-    <ul>
-      {navLinks.map((link) => (
-        <li key={link.title}>
-          <Link
-            href={link.path}
-            className={pathName === link.path ? 'activeLink' : ''}
-            passHref
-          >
-            {link.title}
+    <ul className="relative flex justify-around bg-slate-700 pb-5 pt-5 rounded-md">
+      {navLinks.map(({ title, path }) => (
+        <li className={pathName === path ? 'activeLink' : ''} key={title}>
+          <Link href={path} passHref>
+            {title}
           </Link>
         </li>
       ))}
@@ -28,4 +25,4 @@ const Nav: React.FC = () => {
   );
 };
 
-export default Nav;
+export default memo(Nav);
