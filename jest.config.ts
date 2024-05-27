c8fb586e61501c973f75f91db65d00fbf2033632
Max Morris <morrm046@gmail.com>
2024-05-27 11:42:30 +0100

    Testing: Update testing config based on NextJS with Jest guide - Change directory and structure for test cases and rename files to use more appropriate filenames - Update jest.config.ts - Add a jest.setup.ts - Set compiler option jsx to preserve

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,17 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
 
-import type {Config} from '@jest/types';
-// Sync object
-const config: Config.InitialOptions = {
-  verbose: true,
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-};
-export default config;
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
