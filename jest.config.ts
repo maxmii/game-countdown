86c7026fc164f93a44bc8eb9ceba05f0fe575cee
Max Morris <morrm046@gmail.com>
2024-05-29 18:52:17 +0100

    Refactor: Change Jest config to sort out import alias

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,14 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+import { compilerOptions } from './tsconfig.json'
+import { pathsToModuleNameMapper, JestConfigWithTsJest } from 'ts-jest'
 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
-
-// Add any custom config to be passed to Jest
-const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    moduleDirectories: ['node_modules', '<rootDir>'],
+    moduleNameMapper: {
+        '^@/components/(.*)$': '<rootDir>/src/components/$1',
+        '^@/app/(.*)$': '<rootDir>/src/app/$1'
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 }
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
